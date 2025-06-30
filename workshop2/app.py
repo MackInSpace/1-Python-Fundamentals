ea25edb438dc9fefc4b54319dda08691093d565a
MackInSpace <amackey@lssu.edu>
2025-06-29 20:21:22 -0600

    Task 3 done

--- workshop2/app.py
+++ workshop2/app.py
@@ -16,3 +16,18 @@ pin = input("Enter PIN: ")
 balance = 0
 
 print(name + " has been registered with a starting balance of $" + str(balance))
+
+while True:
+    print("LOGIN")
+    name_to_validate = input("Enter name: ")
+    pin_to_validate = input("Enter PIN: ")
+
+    if name_to_validate == name and pin_to_validate == pin:
+        print("Login successful!")
+        break
+    else:
+        print("Invalid Credentials!")
+
+while True: 
+    atm_menu(name)
+    option = input("Choose an option:  ")
