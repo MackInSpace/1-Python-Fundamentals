acc353f4d312c9cf40dd40124a653e4edab68ea0
MackInSpace <amackey@lssu.edu>
2025-07-06 12:57:33 -0600

    bonus tasks complete

--- week3/workshop3/donations_pkg/homepage.py
+++ week3/workshop3/donations_pkg/homepage.py
@@ -10,15 +10,33 @@ def show_homepage():
     print("------------------------------------------")
 
 def donate(username):
-    donation_amt = input("\nEnter amount to donate: ")
-    donation_string = f"{username} donated ${donation_amt}"
-    print("Thank you for your donation!")
-    return donation_string
+    while True:
+        donation_amt = input("Enter amount to donate: ")
+
+        try:
+            amount = float(donation_amt)
+            if amount <= 0:
+                print("Donation amount must be greater than $0.")
+            else:
+                donation_string = f"{username} donated ${amount:.2f}"
+                print("Thank you for your generosity!")
+                return donation_string
+        except ValueError:
+            print("Please enter a valid numeric donation amount.")
 
 def show_donations(donations):
     print("\n--- All Donations ---")
     if len(donations) == 0:
         print("Currently, there are no donations.")
     else:
+        total = 0.0
         for donation in donations:
             print(donation)
+
+        try: 
+            amount = float(donation.split("$")[1])
+            total += amount
+        except (IndexError, ValueError):
+            print("Could not parse donation amount.")
+
+    print(f"\nTotal amount donated: ${total:.2f}")
--- week3/workshop3/donations_pkg/user.py
+++ week3/workshop3/donations_pkg/user.py
@@ -1,8 +1,15 @@
 def login(database, username, password):
-    if username in database:
-        if database[username] == password:
+    username_lower = username.lower()
+    if len(username) > 10:
+        print("\nUsername must be 10 characters or less.")
+        return ""
+    if len(password) < 5:
+        print("\nPassword must be 5 characters or more.")
+        return ""
+    if username_lower in database:
+        if database[username_lower] == password:
             print("\nWelcome back admin!")
-            return username
+            return username_lower
         else: 
             print("Incorrect password for admin.")
             return ""
@@ -11,9 +18,10 @@ def login(database, username, password):
         return ""
     
 def register(database, username):
-    if username in database:
+    username_lower = username.lower()
+    if username_lower in database:
         print("\nUser already registered.")
         return ""
     else:
         print(f"\nUsername {username} registered!")
-        return username
+        return username_lower
