29e5f1f1b7cd833f9b4e1b6204563478e6c419f2
MackInSpace <amackey@lssu.edu>
2025-07-06 12:04:45 -0600

    task 3 complete

--- week3/workshop3/app.py
+++ week3/workshop3/app.py
@@ -9,3 +9,27 @@ if authorized_user == "":
     print("You must be logged in to donate.")
 else:
     print(f"Logged in as {authorized_user}")
+
+while True:
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        print("TODO: Write Login Functionality")
+    elif choice == "2":
+        print("TODO: Write Register Functionality")
+    elif choice == "3":
+        print("TODO: Write Donate Functionality")
+    elif choice == "4":
+        print("TODO: Write Show Donations Functionality")
+    elif choice == "5":
+        print("Leaving DonateMe...")
+        break
+    else: 
+        print("Invalid option. Please choose a valid number from the menu.")
+
+    print("\n")
+    show_homepage()
+    if authorized_user == "":
+        print("You must be logged in to donate.")
+    else:
+        print(f"Logged in as {authorized_user}")
