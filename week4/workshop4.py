24dc681dbed9f84ccb01785f0dbdebe030016b79
MackInSpace <amackey@lssu.edu>
2025-07-13 19:45:41 -0600

    workshop4 task 1

---
+++ week4/workshop4.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, name, pin, password):
+        self.name = name
+        self.pin = pin
+        self.password = password
+
+"""Driver Code for Task 1"""
+""" user1 = User("Bob", 1234, "password")
+print(user1.name, user1.pin, user1.password) """
+
+    
