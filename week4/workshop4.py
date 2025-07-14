e50439eaa764bada10db7426402ea49e1fccdad6
MackInSpace <amackey@lssu.edu>
2025-07-13 20:12:41 -0600

    workshop4 task 4

--- week4/workshop4.py
+++ week4/workshop4.py
@@ -18,6 +18,15 @@ class BankUser(User):
         super().__init__(name, pin, password)
         self.balance = 0
 
+    def show_balance(self):
+        print("Bob has an account balance of:", self.balance)
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def deposit(self, amount):
+        self.balance += amount
+
 """ Driver Code for Task 1 """
 # user1 = User("Bob", 1234, "password")
 # print(user1.name, user1.pin, user1.password)
@@ -35,3 +44,13 @@ class BankUser(User):
 """ Driver Code for Task 3 """
 # user3 = BankUser("Bob", 1234, "password")
 # print(user3.name, user3.pin, user3.password, user3.balance)
+
+""" Driver Code for Task 4 """
+# bank_user2 = BankUser("Bob", 1234, "password")
+# bank_user2.show_balance()
+
+# bank_user2.deposit(1000.0)
+# bank_user2.show_balance()
+
+# bank_user2.withdraw(500.0)
+# bank_user2.show_balance()
