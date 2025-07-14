837e62266535c103d3086fe0b70766ffc3d9d1d4
MackInSpace <amackey@lssu.edu>
2025-07-13 20:05:55 -0600

    workshop4 taks 3

--- week4/workshop4.py
+++ week4/workshop4.py
@@ -13,17 +13,25 @@ class User:
     def change_password(self, new_password):
         self.password = new_password
 
+class BankUser(User):
+    def __init__(self, name, pin, password):
+        super().__init__(name, pin, password)
+        self.balance = 0
 
 """ Driver Code for Task 1 """
 # user1 = User("Bob", 1234, "password")
 # print(user1.name, user1.pin, user1.password)
 
 """ Driver Code for Task 2 """
-user2 = User("Bob", 1234, "password")
-print(user2.name, user2.pin, user2.password)
+# user2 = User("Bob", 1234, "password")
+# print(user2.name, user2.pin, user2.password)
 
-user2.change_name("Bobby")
-user2.change_pin(4321)
-user2.change_password("newpassword")
+# user2.change_name("Bobby")
+# user2.change_pin(4321)
+# user2.change_password("newpassword")
 
-print(user2.name, user2.pin, user2.password)
+# print(user2.name, user2.pin, user2.password)
+
+""" Driver Code for Task 3 """
+# user3 = BankUser("Bob", 1234, "password")
+# print(user3.name, user3.pin, user3.password, user3.balance)
