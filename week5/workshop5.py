175711ff18f14e661087190fd7cd5a08179e45b4
MackInSpace <amackey@lssu.edu>
2025-07-20 21:04:10 -0600

    task 1 complete

--- week5/workshop5.py
+++ week5/workshop5.py
@@ -4,11 +4,11 @@ def guess_random_number(tries, start, stop):
     target = random.randint(start, stop)
 
     while tries != 0:
-        print(f"Tries remaining: {tries}")
+        print(f"Number of tries left: {tries}")
         guess = int(input(f"Guess a random number between {start} and {stop}: "))
 
         if guess == target:
-            print("Congratulations! You guessed the correct number!")
+            print("You guessed the correct number!")
             return
         elif guess < target:
             print("Guess higher!")
@@ -17,4 +17,7 @@ def guess_random_number(tries, start, stop):
 
         tries -= 1
 
-    print(f"Sorry, you ran out of tries. The correct number was {target}.")
+    print(f"You have failed to guess the number: {target}.")
+
+if __name__ == "__main__":
+    guess_random_number(5, 0, 10)
