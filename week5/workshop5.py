31b88473fdbb58ce46ef498112c8406aea8fd53b
MackInSpace <amackey@lssu.edu>
2025-07-20 21:23:18 -0600

    task 2 done

--- week5/workshop5.py
+++ week5/workshop5.py
@@ -19,5 +19,26 @@ def guess_random_number(tries, start, stop):
 
     print(f"You have failed to guess the number: {target}.")
 
+""" if __name__ == "__main__":
+    guess_random_number(5, 0, 10) """
+
+def guess_random_num_linear(tries, start, stop):
+    target = random.randint(start, stop)
+    print(f"The number for the program to guess is: {target}")
+
+    for guess in range(start, stop + 1):
+        if tries == 0:
+            break
+        
+        print(f"Number of tries left: {tries}")
+        print(f"The program is guessing... {guess}")
+        tries -= 1
+
+        if guess == target:
+            print(f"The program has guessed the correct number!")
+            return
+        
+    print(f"The program has failed to guess the correct number.")
+
 if __name__ == "__main__":
-    guess_random_number(5, 0, 10)
+    guess_random_num_linear(5, 0, 10)
